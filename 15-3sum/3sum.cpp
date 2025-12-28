b522e7d775fca8cf1e23f6e6cc6253173b615161
Anadi Sharma <sharma.anadi007@gmail.com>
2025-12-28 23:39:51 +0530

    Time: 53 ms (36.87%) | Memory: 28.9 MB (98.86%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,27 +4,30 @@ public:
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i] == nums[i-1]){
+        int i,j,k;
+        for(i=0; i<n-2; i++){
+            if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
-            int j = i+1;
-            int k = n-1;
-            
+            j=i+1;
+            k=n-1;
             while(j < k){
-                int total = nums[i] + nums[j] + nums[k];
-                if(total > 0){
-                    k--;
-                }
-                else if(total < 0){
-                    j++;
-                }
-                else{
+                if(nums[j] + nums[k] == -nums[i]){
                     res.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
-                    while(j < n && nums[j]==nums[j-1]){ j++ ;}
-                    while(k > 0 && nums[k] == nums[k+1]){ k--;}
+                    while(nums[j] == nums[j-1] && j<n-1){
+                        j++;
+                    }
+                    while(nums[k] == nums[k+1] && k>0){
+                        k--;
+                    }
+                }
+                else if(nums[j] + nums[k] > -nums[i]){
+                    k--;
+                }
+                else{
+                    j++;
                 }
             }
         }
